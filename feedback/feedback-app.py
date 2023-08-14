a42674f3a86a6a9e19e2b99b6eb411e200861b49
Joshua Carroll <joshua.carroll@snowflake.com>
2023-08-14 13:31:19 -0700

    Fix feedback re-render bug

--- feedback/feedback-app.py
+++ feedback/feedback-app.py
@@ -3,11 +3,9 @@ import openai
 import trubrics
 from streamlit_feedback import streamlit_feedback
 
-st.title("🔎 Chat with user feedback (Trubrics)")
+st.title("🔎 Chat with user feedback")
 
-"""A simple app using [streamlit-feedback](https://github.com/trubrics/streamlit-feedback)
-from Trubrics. **Note:** Has a bug currently where the feedback will reset. Try clicking it again if this happens.
-We're working on a fix :)
+"""A simple app using [streamlit-feedback](https://github.com/trubrics/streamlit-feedback).
 """
 
 if "authorized" not in st.session_state:
@@ -20,45 +18,16 @@ if "authorized" not in st.session_state:
     st.session_state.authorized = True
     st.experimental_rerun()
 
+openai.api_key = st.secrets.openai_api_key
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hi, how can I help you?"}]
-
-openai.api_key = st.secrets.openai_api_key
-config = trubrics.init(
-    email=st.secrets.TRUBRICS_EMAIL,
-    password=st.secrets.TRUBRICS_PASSWORD,
-)
 
 with st.sidebar:
     feedback_type = st.selectbox("Type of feedback", ["thumbs", "faces"])
     include_text = st.checkbox("Enable text feedback?")
 
-
-def get_feedback(idx):
-    label = None
-    if include_text:
-        label = "[Optional] Please provide an explanation"
-    feedback = streamlit_feedback(
-        feedback_type=feedback_type,
-        optional_text_label=label,
-        key=f"feedback_{idx}",
-    )
-    if feedback:
-        collection = trubrics.collect(
-            component_name="default",
-            model="gpt",
-            response=feedback,
-            metadata={"chat": st.session_state.messages[0:idx]},
-        )
-        trubrics.save(config, collection)
-        st.toast("Feedback recorded!", icon="📝")
-
-
-for idx, msg in enumerate(st.session_state.messages):
-    with st.chat_message(msg["role"]):
-        st.write(msg["content"])
-        if msg["role"] == "assistant" and idx > 0:
-            get_feedback(idx)
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input(placeholder="Tell me a joke about sharks"):
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -67,6 +36,28 @@ if prompt := st.chat_input(placeholder="Tell me a joke about sharks"):
     r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     response = r.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": response})
-    with st.chat_message("assistant"):
-        st.write(response)
-        get_feedback(len(st.session_state.messages))
+    st.chat_message("assistant").write(response)
+
+# At least one generated response, basically
+if len(st.session_state.get("messages", [])) > 2:
+    config = trubrics.init(
+        email=st.secrets.TRUBRICS_EMAIL,
+        password=st.secrets.TRUBRICS_PASSWORD,
+    )
+    label = None
+    if include_text:
+        label = "[Optional] Please provide an explanation"
+    feedback = streamlit_feedback(
+        feedback_type=feedback_type,
+        optional_text_label=label,
+        key=f"feedback_{len(st.session_state.messages)}",
+    )
+    if feedback:
+        collection = trubrics.collect(
+            component_name="default",
+            model="gpt",
+            response=feedback,
+            metadata={"chat": st.session_state.messages},
+        )
+        trubrics.save(config, collection)
+        st.toast("Feedback recorded!", icon="📝")
